deae5c341e5a169becfab8a911dac61113fd464d
Sleimane-Hemida <23078@supnum.mr>
2026-04-18 22:41:04 +0000

    je sais meme pas y'a quoi

--- backend/src/config/initDb.js
+++ backend/src/config/initDb.js
@@ -14,7 +14,7 @@ async function initializeDatabase() {
     return true;
   } catch (error) {
     console.error('❌ Database connection error:', error.message);
-    return false;
+    throw error;
   }
 }
 
